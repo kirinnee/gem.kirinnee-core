add4d5dc450782f47d0462331539240d96f1689b
kirinnee <kirinnee97@gmail.com>
2019-03-02 15:26:15 +0800

    [Feat] added skip to enumerable
    skip skips the first x element of the enumerable

--- lib/kirinnee_core/enumerable.rb
+++ lib/kirinnee_core/enumerable.rb
@@ -23,4 +23,18 @@ module Enumerable
 	def where!(&f)
 		select! &f
 	end
+
+	# Skips the first x elements of the enumerable
+	# Does not mutate the original enumerable
+	#
+	# [1,2,3,4,5].skip 3 #=> [4,5]
+	# {:a=>1, :b=>2, :c=>3, :d=>4, :e=>5}.skip 3 #=> {:d=>4, :e=>5}
+	#
+	#
+	# @param [Integer] x the number of elements to skip
+	# @return [Enumerable]
+	def skip(x)
+		drop(x)
+	end
+
 end
--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -27,4 +27,30 @@ describe 'Enumerable' do
 			expect(filtered).to eq [1, 3]
 		end
 	end
+
+	describe "skip" do
+		it 'should skip the first x elements' do
+			expect([1, 2, 3, 4, 5].skip 3).to eq [4, 5]
+			expect({:a => 1, :b => 2, :c => 3, :d => 4, :e => 5}.skip(3).to_h).to eq({:d => 4, :e => 5})
+		end
+
+		it "should return empty enumerable if x is larger than enumerable size" do
+			expect([1, 2, 3].skip 4).to eq []
+			expect({:a => 1, :b => 2, :c => 3}.skip(100).to_h).to eq({})
+		end
+
+		it 'should return original enumerable if x is 0' do
+			expect([1, 2, 3].skip 0).to eq [1, 2, 3]
+			expect({:a => 1, :b => 2, :c => 3}.skip(0).to_h).to eq({:a => 1, :b => 2, :c => 3})
+		end
+
+		it "should not modify original enumerable" do
+			original = [1, 2, 3, 4, 5]
+			skipped = original.skip(2)
+			expect(original).to eq [1, 2, 3, 4, 5]
+			expect(skipped).to eq([3, 4, 5])
+		end
+
+	end
+
 end
